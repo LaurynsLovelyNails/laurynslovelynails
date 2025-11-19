2344b5cc7e29d549c78f4b61eb5728a90dbd9f6d
LaurynsLovelyNails <snoopylover545@gmail.com>
2025-11-19 14:13:10 -0500

    Create products.js

---
+++ products.js
@@ -0,0 +1,24 @@
+// products.js
+const products = [
+  {
+    name: "Pretty Little Lady",
+    price: 20, // update price here
+    img: "images/design1.png"
+  },
+  {
+    name: "Very Hungry Caterpillar",
+    price: 20,
+    img: "images/design2.png"
+  },
+  {
+    name: "Glitter Dream",
+    price: 13,
+    img: "images/design3.png"
+  },
+  {
+    name: "Rosy Edge",
+    price: 13,
+    img: "images/design4.png"
+  }
+  // add more designs here
+];
